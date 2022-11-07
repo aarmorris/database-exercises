681709b02988bf156e0f549372071c1f41822a76
Aaron Morris <aaron.r.morris3@gmail.com>
2022-11-07 15:53:52 -0600

    selection-finish

---
+++ delete_exercises.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+
+DELETE FROM albums WHERE release_date > 1991;
+
+DELETE albums FROM albums WHERE genre = 'disco';
+
+DELETE albums FROM albums WHERE name = 'Whitney Houston';
+
+
+
+
